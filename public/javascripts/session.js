08c8c5bd8818e59f5c1eb46df7d3ac534bd0c3f6
Marc <marc.gruenew@gmail.com>
2016-04-11 19:39:50 +0200

    make shit responsive yo

--- public/javascripts/session.js
+++ public/javascripts/session.js
@@ -169,10 +169,4 @@
             event.preventDefault;
         });
     }
-
-    $(document).ready(function () {
-        // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
-        $('.modal-trigger').leanModal();
-    });
-
 })(window, jQuery);
